bb6b7639eb0f1a313328d57a62f4a0ad2c473051
RachanaVenati <168229772+RachanaVenati@users.noreply.github.com>
2024-05-01 19:45:34 +0000

    final

--- authorship-verification-bayes/run.py
+++ authorship-verification-bayes/run.py
@@ -5,24 +5,18 @@ from tira.rest_api_client import Client
 from tira.third_party_integrations import get_output_directory
 import re
 if __name__ == "__main__":
-    def preprocessingText(text):
-        # Lowercasing
+    def Convert_lower_remove_punctuation(text):
         text = text.lower()
-        # Removing punctuation
         text = re.sub(r'[^\w\s]', '', text)
         return text
 
-    # Custom stemming function
-    def stem_word(word):
-        #  suffix stripping
+    def Suffix_Removal(word):
         if word.endswith("ing"):
             return word[:-3]
         else:
             return word
 
-    # Custom lemmatization function
-    def lemmatize_word(word):
-        # Basic lemmatization (e.g., removing 's', 'es', 'ed', 'ing')
+    def Normalize(word):
         if word.endswith("s") or word.endswith("es"):
             return word[:-1]
         elif word.endswith("ed"):
@@ -30,8 +24,7 @@ if __name__ == "__main__":
         else:
             return word
 
-    #stop word removal function
-    def remove_stop_words(tokens):
+    def Stop_words_removal(tokens):
         stop_words = set(["a", "an", "the", "and", "or", "but", "for", "of", "in", "on", "at", "to", "from", "with"])  # Add more stop words as needed
         return [token for token in tokens if token not in stop_words]
 
@@ -39,11 +32,11 @@ if __name__ == "__main__":
         return text.split()
 
     def preprocess(text):
-        text = preprocessingText(text)
+        text = Convert_lower_remove_punctuation(text)
         tokens = tokenize_text(text)
-        stemmed_tokens = [stem_word(token) for token in tokens]
-        lemmatized_tokens = [lemmatize_word(token) for token in tokens]
-        tokens_without_stop_words = remove_stop_words(lemmatized_tokens)
+        stemmed_tokens = [Suffix_Removal(token) for token in tokens]
+        lemmatized_tokens = [Normalize(token) for token in tokens]
+        tokens_without_stop_words = Stop_words_removal(lemmatized_tokens)
         processed_text = ' '.join(tokens_without_stop_words)
         return processed_text
 
